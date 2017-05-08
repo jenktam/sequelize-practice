2c3429c69a690411877489fbf2ce62f0acc59b69
Jen Tam <jennifer.k.tam@gmail.com>
2017-05-07 19:43:19 -0700

    all but incrementing haveBirthday age on instance method

--- models/user.js
+++ models/user.js
@@ -2,9 +2,62 @@ const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/sequelize_practice', { logging: false });
 
 const User = db.define('user', {
-// YOUR CODE HERE...
+  first: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    }
+  },
+  last: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    }
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 18
+    }
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  bio: {
+    type: Sequelize.TEXT,
+    allowNull: true,
+  },
 }, {
-// ...AND HERE
+  getterMethods: {
+    fullName: function(){
+      return `${this.first} ${this.last}`;
+    }
+  },
+  // couldn't get instance method to save new age
+  instanceMethods: { //this refers to the instance
+    haveBirthday: function(){
+      return User.findAll({
+        where: {
+          age: this.age
+        },
+      })
+      .then( (foundUser) => {
+        var value = foundUser[0].dataValues.age;
+        return this.age += 1;
+      })
+   }
+  },
+  hooks: {
+    beforeValidate: function(user) {
+      if (user.haveBirthday()) {
+        user.age += 1;
+      }
+    }
+  }
 });
 
 module.exports = User;
